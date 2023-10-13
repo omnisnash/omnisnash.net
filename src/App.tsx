6d8f6ce17295f588aa366eb8746cb32655f9dfce
Clément Garin <clement.garin@outlook.fr>
2023-10-13 16:29:34 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hey! ✨</h1>
+      <div className="card">
+          <p className="read-the-docs">
+              It's empty there, maybe I will add something someday?
+          </p>
+      </div>
+
+    </>
+  )
+}
+
+export default App
